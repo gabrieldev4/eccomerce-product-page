cfb25462e2f3d7b667eaf15d346d70f4b6c24538
gabriel <gabrielminima@gmail.com>
2024-10-08 13:50:03 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': 'hsl(26, 100%, 55%)',
+      'very-dark-blue': 'hsl(220, 13%, 13%)',
+      'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+      'grayish-blue': 'hsl(220, 14%, 75%)',
+      'light-grayish-blue': 'hsl(223, 64%, 98%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'black': 'hsl(0, 0%, 0%)',
+      'pale-orange': 'hsl(25, 100%, 94%)',
+      extends:[]
+    },
+  },
+  plugins: [],
+}
+
